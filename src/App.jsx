42dfd3df1512e4a9cf44241cf8811d1780727e3f
obaydul_Kuasha <obaydul00000@gmail.com>
2024-01-26 14:30:50 +0600

    start a new restaurent , then file  set up and start more code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat neque labore libero minima, fugit animi ea omnis natus sit? Saepe est magnam corrupti repudiandae officia incidunt voluptatibus quaerat laudantium amet.</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,44 @@
+
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import img1 from '../../../assets/home/01.jpg';
+import img2 from '../../../assets/home/02.jpg';
+import img3 from '../../../assets/home/03.png';
+import img4 from '../../../assets/home/04.jpg';
+import img5 from '../../../assets/home/05.png';
+import img6 from '../../../assets/home/06.png';
+
+
+const Banner = () => {
+    return (
+        <Carousel>
+            <div>
+                <img src={img1} />
+                <p className="legend">Obaydur</p>
+            </div>
+            <div>
+                <img src={img2} />
+                <p className="legend">Rahman</p>
+            </div>
+            <div>
+                <img src={img3} />
+                <p className="legend">Rifat</p>
+            </div>
+            <div>
+                <img src={img4} />
+                <p className="legend">Kuasha</p>
+            </div>
+            <div>
+                <img src={img5} />
+                <p className="legend">Tayfur</p>
+            </div>
+            <div>
+                <img src={img6} />
+                <p className="legend"> Sorder</p>
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
